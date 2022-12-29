4556922e569671d143deb0c209a6fa6bd25b7829
adammizell <32174533+amm150@users.noreply.github.com>
2022-12-29 14:57:07 +0000

    fix name mismatch

--- src/apis/nhlPlayerStats.ts
+++ src/apis/nhlPlayerStats.ts
@@ -4,101 +4,100 @@ import type { OddsPlayerData } from "./nhlShotOdds";
 import type { PlayerData } from "./nhlActiveRoster";
 
 export interface PlayerStats {
-    [key: string]: any,
-    name: string,
-    line: number,
-    mean: number,
-    median: number,
-    over: number,
-    under: number,
-    rating: number,
-    team: string,
-    opponent: string,
-    allowed: number,
-    allowedRank: string,
-    powerPlaysRank: string,
+  [key: string]: any,
+  name: string,
+  line: number,
+  mean: number,
+  median: number,
+  over: number,
+  under: number,
+  rating: number,
+  team: string,
+  opponent: string,
+  allowed: number,
+  allowedRank: string,
+  powerPlaysRank: string,
 }
 
 interface GameData {
-    stat: {
-        assists: number,
-        shots: number,
-        points: number,
-        goals: number,
-        hits: number,
-        blocked: number,
-    }
+  stat: {
+    assists: number,
+    shots: number,
+    points: number,
+    goals: number,
+    hits: number,
+    blocked: number,
+  }
 }
 
 interface PlayerGameData {
-    splits: GameData[],
+  splits: GameData[],
 }
 
 export default async function getNHLPlayerStats(names: OddsPlayerData[], players: PlayerData, identifier: keyof GameData['stat'], gameCount: number) {
-    let playerStats: PlayerStats[] = [];
-    const season = calculateSeason();
+  let playerStats: PlayerStats[] = [];
+  const season = calculateSeason();
 
-    for (const name in names) {
-        const playerName = names[name].name;
-        const playerOdds = Number(names[name].line);
-        const {
-            id: playerId,
-            name: playerNameFormatted,
-        } = getPlayerId(playerName, players);
+  for (const name in names) {
+    const playerName = names[name].name;
+    const playerOdds = Number(names[name].line);
+    const {
+      id: playerId,
+      name: playerNameFormatted,
+    } = getPlayerId(playerName, players);
 
-        if(!playerId) {
-            continue;
-        }
-
-        const {
-            splits
-        } = await nhlApi.getPlayer({
-            id: playerId,
-            stats: "gameLog",
-            season: season,
-        }) as unknown as PlayerGameData;
-
-        const lastNGames = splits.slice(0, gameCount);
-        const dataArr = lastNGames.map((gameData: GameData) => {
-            return gameData.stat[identifier];
-        }, []);
-
-        if(dataArr.length !== gameCount) {
-            continue;
-        }
-
-        const mean =  getMean(dataArr);
-        const median = getMedian(dataArr);
-        const over = dataArr.filter(value => value > playerOdds).length;
-        const under = dataArr.filter(value => value < playerOdds).length;
-        const rating =  over + (mean - playerOdds) + (median - playerOdds);
-        const {
-            shotsAllowed,
-            goalsAllowed,
-            shotsAllowedRank,
-            goalsAllowedRank,
-            powerPlaysAllowedRank,
-            team,
-            opponent,
-        } = players[playerNameFormatted];
-        const isShots = identifier === "shots";
-    
-
-        playerStats.push({
-            name: playerName,
-            line: playerOdds,
-            mean,
-            median,
-            over,
-            under,
-            rating,
-            team,
-            opponent,
-            allowed: isShots ? shotsAllowed : goalsAllowed,
-            allowedRank: isShots ? shotsAllowedRank : goalsAllowedRank,
-            powerPlaysRank: powerPlaysAllowedRank,
-        });
+    if (!playerId) {
+      continue;
     }
-    
-    return playerStats;
+
+    const {
+      splits
+    } = await nhlApi.getPlayer({
+      id: playerId,
+      stats: "gameLog",
+      season: season,
+    }) as unknown as PlayerGameData;
+
+    const lastNGames = splits.slice(0, gameCount);
+    const dataArr = lastNGames.map((gameData: GameData) => {
+      return gameData.stat[identifier];
+    }, []);
+
+    if (dataArr.length !== gameCount) {
+      continue;
+    }
+
+    const mean = getMean(dataArr);
+    const median = getMedian(dataArr);
+    const over = dataArr.filter(value => value > playerOdds).length;
+    const under = dataArr.filter(value => value < playerOdds).length;
+    const rating = over + (mean - playerOdds) + (median - playerOdds);
+    const {
+      shotsAllowed,
+      goalsAllowed,
+      shotsAllowedRank,
+      goalsAllowedRank,
+      powerPlaysAllowedRank,
+      team,
+      opponent,
+    } = players[playerNameFormatted];
+    const isShots = identifier === "shots";
+
+    playerStats.push({
+      name: playerName,
+      line: playerOdds,
+      mean,
+      median,
+      over,
+      under,
+      rating,
+      team,
+      opponent,
+      allowed: isShots ? shotsAllowed : goalsAllowed,
+      allowedRank: isShots ? shotsAllowedRank : goalsAllowedRank,
+      powerPlaysRank: powerPlaysAllowedRank,
+    });
+  }
+
+  return playerStats;
 };
--- src/utils.ts
+++ src/utils.ts
@@ -4,124 +4,128 @@ import type { PlayerData, RosterData } from './apis/nhlActiveRoster';
 import type { PlayerStats } from './apis/nhlPlayerStats';
 
 export const calculateSeason = () => {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth();
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth();
 
-    // season should be the year that the season started and ended. So for 2022/2023 season this should be "20222023".
-    const season = month > 7 ? `${year}${year + 1}` : `${year - 1}${year}`;
+  // season should be the year that the season started and ended. So for 2022/2023 season this should be "20222023".
+  const season = month > 7 ? `${year}${year + 1}` : `${year - 1}${year}`;
 
-    return season;
+  return season;
 }
 
 // Some names are different between the NHL Api and Draftkings....
 const nameMap = [
-    ['Matthew', 'Matt']
+  ['Matthew', 'Matt']
 ];
 
 export const getPlayerId = (name: string, players: PlayerData) => {
-    const formattedName = name.replace(/ *\([^)]*\) */g, "");
+  const formattedName = name.replace(/ *\([^)]*\) */g, "");
 
-    try {
-        const playerId = getPlayerByName(formattedName);
+  try {
+    const playerId = getPlayerByName(formattedName);
 
-        if(Array.isArray(playerId)) {
-            throw new Error(`Found multiple player IDs for ${formattedName}`)
-        }
-
-        return {
-            id: playerId,
-            name: formattedName,
-        };
-    } catch (e) {
-        console.log(e);
-        let playerId = players[formattedName]?.id;
-        let playerName = formattedName;
-
-        if(!playerId) {
-            const fullName = formattedName.split(' ');
-            const firstName = fullName[0];
-            const lastName = fullName[1];
-    
-            for(const i in nameMap) {
-                if(nameMap[i][0] === firstName) {
-                    const updatedName = `${nameMap[i][1]} ${lastName}`;
-                    playerId = players[updatedName]?.id;
-                    playerName = updatedName;
-                    break;
-                } else if (nameMap[i][1] === firstName) {
-                    const updatedName = `${nameMap[i][0]} ${lastName}`;
-                    playerId = players[updatedName]?.id;
-                    playerName = updatedName;
-                    break;
-                }
-            }
-        }
-
-        return {
-            id: playerId,
-            name: playerName,
-        };
+    if (Array.isArray(playerId)) {
+      throw new Error(`Found multiple player IDs for ${formattedName}`)
     }
+
+    if (!players[formattedName]) {
+      throw new Error(`Player name does NOT match ${formattedName}`)
+    }
+
+    return {
+      id: playerId,
+      name: formattedName,
+    };
+  } catch (e) {
+    console.log(e);
+    let playerId = players[formattedName]?.id;
+    let playerName = formattedName;
+
+    if (!playerId) {
+      const fullName = formattedName.split(' ');
+      const firstName = fullName[0];
+      const lastName = fullName[1];
+
+      for (const i in nameMap) {
+        if (nameMap[i][0] === firstName) {
+          const updatedName = `${nameMap[i][1]} ${lastName}`;
+          playerId = players[updatedName]?.id;
+          playerName = updatedName;
+          break;
+        } else if (nameMap[i][1] === firstName) {
+          const updatedName = `${nameMap[i][0]} ${lastName}`;
+          playerId = players[updatedName]?.id;
+          playerName = updatedName;
+          break;
+        }
+      }
+    }
+
+    return {
+      id: playerId,
+      name: playerName,
+    };
+  }
 }
 
 export const getMedian = function(array: number[]) {
-    array.sort(function(a, b) {
-      return a - b;
-    });
-    var mid = array.length / 2;
-    return mid % 1 ? array[mid - 0.5] : (array[mid - 1] + array[mid]) / 2;
+  array.sort(function(a, b) {
+    return a - b;
+  });
+  var mid = array.length / 2;
+  return mid % 1 ? array[mid - 0.5] : (array[mid - 1] + array[mid]) / 2;
 };
 
 export const getMean = (array: number[]) => Math.round((array.reduce((a, b) => a + b) / array.length) * 100) / 100;
 
 export const buildMessageEmbed = (message: Message, playerStats: PlayerStats[], gameCount: string, command: string) => {
-    const footerText = message.author.tag;
-    const footerIcon = message.author.displayAvatarURL();
-    const embed = new MessageEmbed()
-      .setTitle(`${command.toLocaleUpperCase()} Data for last ${gameCount} games`)
-      .setColor('GREEN')
-      .setFooter({ text: footerText, iconURL: footerIcon });
+  const footerText = message.author.tag;
+  const footerIcon = message.author.displayAvatarURL();
+  const embed = new MessageEmbed()
+    .setTitle(`${command.toLocaleUpperCase()} Data for last ${gameCount} games`)
+    .setColor('GREEN')
+    .setFooter({ text: footerText, iconURL: footerIcon });
 
-    playerStats.forEach((playerStat) => {
-        embed.addField(
-            `${playerStat.name} - (${playerStat.team} vs. ${playerStat.opponent})`,
-            `Line:   ${playerStat.line}
+  playerStats.forEach((playerStat) => {
+    embed.addField(
+      `${playerStat.name} - (${playerStat.team} vs. ${playerStat.opponent})`,
+      `Line:   ${playerStat.line}
             Over:   ${playerStat.over}
             Under:  ${playerStat.under}
             Mean:  ${playerStat.mean}
-            Allowed Per Game:  ${Math.round(playerStat.allowed *100) / 100}
+            Allowed Per Game:  ${Math.round(playerStat.allowed * 100) / 100}
             Allowed Rank:  ${playerStat.allowedRank}
             Powerplay Allowed Rank:  ${playerStat.powerPlaysRank}`
-        )
-    });
+    )
+  });
 
-    return embed;
+  return embed;
 }
 
 export interface TeamStats {
-    shotsAllowed: number,
-    goalsAllowed: number,
-    shotsAllowedRank: string,
-    powerPlaysAllowedRank: string,
-    goalsAllowedRank: string,
-    team: string,
-    opponent: string,
+  shotsAllowed: number,
+  goalsAllowed: number,
+  shotsAllowedRank: string,
+  powerPlaysAllowedRank: string,
+  goalsAllowedRank: string,
+  team: string,
+  opponent: string,
 }
 
 export const getSkatersFromRoster = (players: RosterData, teamData: TeamStats) => {
-    let skaters: PlayerData = {};
-    players.reduce((prevPlayers: PlayerData, curPlayer) => {
-        // Ignore goalies
-        if (curPlayer.position.code !== "G") {
-            skaters[curPlayer.person.fullName] = {
-                id: curPlayer.person.id,
-                ...teamData,
-            };
-        }
-    
-        return prevPlayers;
-    }, {});
+  let skaters: PlayerData = {};
+  players.reduce((prevPlayers: PlayerData, curPlayer) => {
+    // Ignore goalies
+    if (curPlayer.position.code !== "G") {
+      skaters[curPlayer.person.fullName] = {
+        id: curPlayer.person.id,
+        ...teamData,
+      };
+    }
 
-    return skaters;
+    return prevPlayers;
+  }, {});
+
+  return skaters;
 }
